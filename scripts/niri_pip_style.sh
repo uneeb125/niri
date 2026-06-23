5fc59daa334f901ea1d533453b7ca963367c5663
uneeb125 <uneebkamal125@gmail.com>
2026-06-22 20:27:19 -0500

    fixed a slight black bars 16:9 ratio

--- scripts/niri_pip_style.sh
+++ scripts/niri_pip_style.sh
@@ -10,7 +10,7 @@ if [ "$IS_FLOATING" = "yes" ]; then
 else
     niri msg action move-window-to-floating
     niri msg action set-window-width 25%
-    niri msg action set-window-height 25%
+    niri msg action set-window-height 25.5%
     niri msg action move-floating-window -x "75%" -y "0%"
     niri msg action move-floating-window -x "-3" -y "+3"
 fi
